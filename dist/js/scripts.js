b8d90d41c39717458ce9f0c56f5170b7418477b2
Nahuel Pico <nahuelpico99@gmail.com>
2026-09-08 21:29:26 -0300

    Mantiene el formulario en la pagina principal

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -3,5 +3,33 @@
 * Copyright 2013-2026 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-landing-page/blob/master/LICENSE)
 */
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+document.querySelectorAll('form[data-netlify="true"]').forEach((form) => {
+	form.addEventListener('submit', async (event) => {
+		event.preventDefault();
+
+		const successMessage = form.querySelector('[id="submitSuccessMessage"]');
+		const errorMessage = form.querySelector('[id="submitErrorMessage"]');
+		const submitButton = form.querySelector('button[type="submit"]');
+		const formData = new FormData(form);
+
+		formData.set('form-name', form.getAttribute('name'));
+		submitButton.disabled = true;
+
+		try {
+			await fetch('/', {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+				body: new URLSearchParams(formData).toString(),
+			});
+
+			successMessage.classList.remove('d-none');
+			errorMessage.classList.add('d-none');
+			form.reset();
+		} catch (error) {
+			successMessage.classList.add('d-none');
+			errorMessage.classList.remove('d-none');
+		} finally {
+			submitButton.disabled = false;
+		}
+	});
+});
